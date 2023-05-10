930e49c182f61284eecce4a7fb9d99f5a5044295
Bskasan <b.kasan@yandex.com>
2023-05-10 12:18:59 +0300

    oop 2 class started

---
+++ 7-Python_OOP-2/oop.py
@@ -0,0 +1,16 @@
+import os
+os.system('cls' if os.name=="nt" else 'clear')
+# ---------------------------------------------
+# ---------------------------------------------
+# Python - OOP - 2
+# ---------------------------------------------
+# Encapcullation: 
+# -> Kapsülleme. Aynı amaç için kullanılan değişken ve methodları bir class içinde yazıyor olmsı  ve class içinde tanımlanan private attre erişelmiyor olması.
+# -> Encapsulation is a mechanism of wrapping the data (variables) and code acting on the data (methods) together as a single unit. In encapsulation, the variables of a class will be hidden from other classes, and can be accessed only through the methods of their current class.
+# --------------------------------------------- 
+# Abstraction: Soyutlama. Kodların gizliliği ve birbirinde bağımsız çalışmaları. Abstraction in python is defined as a process of handling complexity by hiding unnecessary information from the user. This is one of the core concepts of object-oriented programming (OOP) languages. That enables the user to implement even more complex logic on top of the provided abstraction without understanding or even thinking about all the hidden background/back-end complexity.
+# ---------------------------------------------
+# Inheritance: Miras Alma. Bir classın tüm özelliklerinin başka bir classa aktarılmasına denir. Inheritance allows us to define a class that inherits all the methods and properties from another class.Parent class is the class being inherited from, also called base class.Child class is the class that inherits from another class, also called derived class.
+# Polymorphism: Miras alınan (inherit edilen) classın özellik/metodlarını yeniden yazılabilyor/değiştirebiliyor olmasına denir. What is Polymorphism: The word polymorphism means having many forms. In programming, polymorphism means the same function name (but different signatures) being used for different types. The key difference is the data types and number of arguments used in function.
+
+# ---------------------------------------------
