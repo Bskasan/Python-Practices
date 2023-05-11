6a33e1fddae94b14032c88ab91ac69ce59634b97
Bskasan <b.kasan@yandex.com>
2023-05-11 23:22:29 +0300

    documents created

--- 8-Django_Intro/myproject/myproject/urls.py
+++ 8-Django_Intro/myproject/myproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for fsdjango project.
+URL configuration for myproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
