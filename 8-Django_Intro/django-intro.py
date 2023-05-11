ee49c72810218756b32f06328b7f18898ea74416
Bskasan <b.kasan@yandex.com>
2023-05-11 22:42:25 +0300

    readme and gitignore added

--- 8-Django_Intro/django-intro.py
+++ 8-Django_Intro/django-intro.py
@@ -5,3 +5,7 @@
 #* A user request for a resource to the Django, Django works as a controller and check to the available resource in URL (urls.py file). if URL maps, a view (function) is called that interact with model and template, it renders a template. Django responds back to the user and sends a template as a response.
 
 #? MVC (Model-View-Controller) is a pattern in software design commonly used to implement user interfaces, data, and controlling logic. It emphasizes a separation between the software's business logic and display. This "separation of concerns" provides for a better division of labor and improved maintenance.
+
+#! FOR LOCAL SETUP (PYTHON DEFAULT SETUP -> GLOBAl)
+#! python -m venv env
+#! source env/Scripts/activate
