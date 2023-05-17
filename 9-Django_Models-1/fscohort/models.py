6f9d060161a6dbe9013ced85abdf7942c022a548
Bskasan <b.kasan@yandex.com>
2023-05-17 15:18:15 +0300

    Student model started

--- 9-Django_Models-1/fscohort/models.py
+++ 9-Django_Models-1/fscohort/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# https://docs.djangoproject.com/en/4.2/topics/db/models/#:~:text=A%20model%20is%20the%20single,.db.models.Model%20.
+
+class Student(models.Model):
+    first_name = 
