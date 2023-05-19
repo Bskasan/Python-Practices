579480aaf29f1bcd94f6523f69cf0f657e0dd9f9
Bskasan <b.kasan@yandex.com>
2023-05-19 23:04:19 +0300

    media url done

--- 10-Django_Models-2/main/settings.py
+++ 10-Django_Models-2/main/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / STATIC_URL
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / MEDIA_URL
 
