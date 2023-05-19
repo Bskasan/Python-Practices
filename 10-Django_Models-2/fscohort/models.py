c67aa89b3777adcd4b42a05c66cd5a29001dc66a
Bskasan <b.kasan@yandex.com>
2023-05-19 22:43:57 +0300

    imagefield

--- 10-Django_Models-2/fscohort/models.py
+++ 10-Django_Models-2/fscohort/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+# https://docs.djangoproject.com/en/4.2/topics/db/models/#:~:text=A%20model%20is%20the%20single,.db.models.Model%20.
+
+class Student(models.Model):
+    # https://docs.djangoproject.com/en/4.2/ref/models/fields/#field-types
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    number = models.IntegerField()
+    picture = models.ImageField(upload_to='images/', default='')
+    is_active = models.BooleanField(default=True)
+    # image = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True) # Write the time automatically when signed up.
+    updated = models.DateTimeField(auto_now=True) # Write the time automaticall when updated.
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} # {self.number}'
+
+    class Meta: # Change Default Settings
+        # https://docs.djangoproject.com/en/4.2/ref/models/options/
+        verbose_name = 'Developer'
+        verbose_name_plural = "Developers"
+        ordering = ["number"] # use '-' for reverse the order 
+
+
