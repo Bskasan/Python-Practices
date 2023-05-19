f127f746f90896e1f05dfd1504f8a17fde3993e0
Bskasan <b.kasan@yandex.com>
2023-05-19 23:07:39 +0300

    image conf settings added

--- 10-Django_Models-2/main/urls.py
+++ 10-Django_Models-2/main/urls.py
@@ -20,3 +20,8 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+#for MEDIA:
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
