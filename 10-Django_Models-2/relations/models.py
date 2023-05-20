6eeb9368656564ab4530647f066542096c6ac55a
Bskasan <b.kasan@yandex.com>
2023-05-20 21:37:23 +0300

    Account and Profile models done and onetoone relations

--- 10-Django_Models-2/relations/models.py
+++ 10-Django_Models-2/relations/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    username = models.CharField(max_length=64)
+    password = models.CharField(max_length=64)
+    email = models.EmailField(max_length=256)
+
+
+    def __str__(self):
+        return f'{self.username}'
+    
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    about = models.TextField()
+    picture = models.ImageField(upload_to='profile/', null=True, blank=True)
+    account = models.OneToOneField(Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.account} - {self.first_name} {self.last_name}'
+
+'''
+on_delete properties:
+    # CASCADE -> if primary deleted, delete foreing too.
+    # SET_NULL -> if primary deleted, set foreign to NULL. (null=True)
+    # SET_DEFAULT -> if primary deleted, set foreing to DEFAULT value. (default='Value')
+    # DO_NOTHING -> if primary deleted, do nothing.
+    # PROTECT -> if foreign is exist, can not delete primary.
+'''
