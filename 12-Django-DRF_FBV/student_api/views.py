47f307d86a9d03449ca04ab0c97712ee2c962422
Bskasan <b.kasan@yandex.com>
2023-05-23 22:07:41 +0300

    postman test started

--- 12-Django-DRF_FBV/student_api/views.py
+++ 12-Django-DRF_FBV/student_api/views.py
@@ -1,8 +1,9 @@
-from rest_framework import Response
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-
+@api_view()
 def home(request):
-    response Response(
+    return Response(
         # To change object to JSON.
         {
             'message': 'Hello World'
