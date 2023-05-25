ed32e40ffa2a610fa24135e04c96ad4a6099625e
Bskasan <b.kasan@yandex.com>
2023-05-25 22:27:00 +0300

    all methods working

--- 13-Django-DRF_CBV/student_api/urls.py
+++ 13-Django-DRF_CBV/student_api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from .views import (
-    StudentListCreate
+    StudentListCreate,
+    StudentDetailUpdateDelete,
 )
 
 # as_view() func -> Class -> Func.
 urlpatterns = [
-    path('student_list', StudentListCreate.as_view()),
+    path('student_list_create/', StudentListCreate.as_view()),
+    path('student_detail_update_delete/<int:pk>', StudentDetailUpdateDelete.as_view()),
 ]
