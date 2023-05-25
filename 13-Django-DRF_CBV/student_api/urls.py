b5aa3cfb14a013afecaf0e409ad9b1c98ae4c54e
Bskasan <b.kasan@yandex.com>
2023-05-25 22:30:58 +0300

    StudentGPPD (one class for all methods)

--- 13-Django-DRF_CBV/student_api/urls.py
+++ 13-Django-DRF_CBV/student_api/urls.py
@@ -3,10 +3,13 @@ from django.urls import path
 from .views import (
     StudentListCreate,
     StudentDetailUpdateDelete,
+    StudentGPPD,
 )
 
 # as_view() func -> Class -> Func.
 urlpatterns = [
     path('student_list_create/', StudentListCreate.as_view()),
     path('student_detail_update_delete/<int:pk>', StudentDetailUpdateDelete.as_view()),
+    path('student_gppd/', StudentGPPD.as_view()),
+    path('student_gppd/<int:pk>', StudentGPPD.as_view()),
 ]
--- 13-Django-DRF_CBV/student_api/views.py
+++ 13-Django-DRF_CBV/student_api/views.py
@@ -48,3 +48,52 @@ class StudentDetailUpdateDelete(APIView):
         student = get_object_or_404(Student, id=pk)
         student.delete()
         return Response({"message" : "Deleted"}, status=status.HTTP_204_NO_CONTENT)
+    
+
+# ----------------------------------------------------------------
+# One Class For All Methods
+
+class StudentGPPD(APIView):
+
+    def get(self, request, pk=0):
+        if pk:
+        # Tek kayıt görüntüle:
+            student = get_object_or_404(Student, id=pk)
+            serializer = StudentSerializer(instance=student)
+            return Response(serializer.data)
+        else:
+        # Kayıtları listele:
+            students = Student.objects.all()
+            serializer = StudentSerializer(instance=students, many=True)
+            return Response(serializer.data)
+    
+    # Yeni Kayıt (POST Method)
+    def post(self, request):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    # Tek kayıt güncelle:
+    def put(self, request, pk):
+        student = get_object_or_404(Student, id=pk)
+        serializer = StudentSerializer(instance=student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    # Tek kayıt sil:
+    def delete(self, request, pk):
+        student = get_object_or_404(Student, id=pk)
+        student.delete()
+        return Response({ "message": "Deleted" }, status=status.HTTP_204_NO_CONTENT)
+
+# ----------------------------------------------------------------
+# GenericAPIView
+# https://www.django-rest-framework.org/api-guide/generic-views/#genericapiview
+# Mixins
+# https://www.django-rest-framework.org/api-guide/generic-views/#mixins
+# ----------------------------------------------------------------
