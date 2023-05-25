213bd190584ee0c5267ab31163bab29424eec334
Bskasan <b.kasan@yandex.com>
2023-05-25 23:04:58 +0300

    router

--- 13-Django-DRF_CBV/student_api/urls.py
+++ 13-Django-DRF_CBV/student_api/urls.py
@@ -19,3 +19,16 @@ urlpatterns = [
     path('student_list_create_api/', StudentListCreateAPIView.as_view()),
     path('student_get_put_delete_api/<int:pk>', StudentRetrieveUpdateDestroyAPIView.as_view()),
 ]
+
+# ----------------------------------------------------------------
+# Router for ModelViewSet
+from .views import StudentMVS
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('students', StudentMVS) # URL sonunda / yok.
+# router.register('another', AnotherMVS)
+# router.register('another', AnotherMVS)
+# router.register('another', AnotherMVS)
+# Add to paths:
+urlpatterns += router.urls
