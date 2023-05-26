725212329f36f68c096a94e5ed41f5422532ce34
Bskasan <b.kasan@yandex.com>
2023-05-26 20:50:10 +0300

    mvs - router done - todoapi

--- 14-TaskTrackerAPI/main/urls.py
+++ 14-TaskTrackerAPI/main/urls.py
@@ -20,3 +20,12 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# MVS = Router
+from rest_framework import routers
+from todo.views import TodoView
+
+router = routers.DefaultRouter()
+router.register(TodoView)
+
+urlpatterns += router.urls
