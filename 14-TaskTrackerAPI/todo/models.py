5d3e5cb202ace1ff68b0f65b1ce17ad70ff8c32f
Bskasan <b.kasan@yandex.com>
2023-05-26 20:29:12 +0300

    written todo model

--- 14-TaskTrackerAPI/todo/models.py
+++ 14-TaskTrackerAPI/todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+
+    # Industry Standart - That means do not touch this code.
+    PRIORITY = (
+        (1, 'High'),
+        (2, 'Medium'),
+        (3, 'Low')
+    )
+
+    title = models.CharField(max_length=64)
+    description = models.TextField(null=True, blank=True)
+    priority = models.PositiveSmallIntegerField(choices=PRIORITY, default=2)
+    is_done = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
