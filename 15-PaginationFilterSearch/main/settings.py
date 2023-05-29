ef410fe315bfda942bf9660b2b984f3747abfe65
Bskasan <b.kasan@yandex.com>
2023-05-29 15:47:19 +0300

    django filters done

--- 15-PaginationFilterSearch/main/settings.py
+++ 15-PaginationFilterSearch/main/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     
     # Modules
     'rest_framework',
+    'django_filters',
     # Apps;
     'todo',
 ]
@@ -137,4 +138,9 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.CursorPagination', #  Kursor ile sayfalama.  (modelde "created" isimli field bekler)
     # 'PAGE_SIZE': 50
+    # 'DEFAULT_FILTER_BACKENDS': [
+    #   'django_filters.rest_framework.DjangoFilterBackend', 
+    #   'rest_framework.filters.SearchFilter',
+    #   'rest_framework.filters.OrderingFilter'
+    # ]
 }
--- 15-PaginationFilterSearch/todo/views.py
+++ 15-PaginationFilterSearch/todo/views.py
@@ -10,6 +10,9 @@ from .paginations import (
     CustomCursorPagination
 )
 
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+
 from .serializers import Todo, TodoSerializer
 
 
@@ -19,6 +22,19 @@ class TodoView(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     pagination_class = CustomPageNumberPagination # Local Pagination Setting.
+
+    # Filtrelemen modulleri
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+
+    # Bire bir eslestirme
+    filterset_fields = ['is_done', 'priority', 'id'] # for django_filters module.
+
+    # Search: İçinde arama:
+    # https://www.django-rest-framework.org/api-guide/filtering/#searchfilter
+    search_fields = ['title', 'description']
+    # Ordering: Sıralama:
+    ordering_fields = ['id', 'title'] # '__all__'
+
     
     # Alternative temporary method
     # PageNumberPagination.page_size = 25
@@ -27,13 +43,13 @@ class TodoView(ModelViewSet):
 
     # Override
     # Manuel Arama Örneği (Override):
-    def get_queryset(self):
-        # URL'den parametre değerini yakala:
-        title = self.request.query_params.get('title')
-        if title is None:
-        # Arama yapma (parametre yok)
-            return super().get_queryset()
-        else:
-        # Arama yap:
-            # queryset içinde ara:
-            return self.queryset.filter(title__contains=title)
+    # def get_queryset(self):
+    #     # URL'den parametre değerini yakala:
+    #     title = self.request.query_params.get('title')
+    #     if title is None:
+    #     # Arama yapma (parametre yok)
+    #         return super().get_queryset()
+    #     else:
+    #     # Arama yap:
+    #         # queryset içinde ara:
+    #         return self.queryset.filter(title__contains=title)
