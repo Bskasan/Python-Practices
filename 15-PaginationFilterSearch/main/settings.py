b78dda8f734d9cda6670009a5c86480b7a64beed
Bskasan <b.kasan@yandex.com>
2023-05-29 14:39:25 +0300

    paginations -2

--- 15-PaginationFilterSearch/main/settings.py
+++ 15-PaginationFilterSearch/main/settings.py
@@ -130,9 +130,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# Rest Framework Global Settings
+# API Global Settings
 REST_FRAMEWORK = {
     # Setup Pagination
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 25
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 25
 }
--- 15-PaginationFilterSearch/todo/views.py
+++ 15-PaginationFilterSearch/todo/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
+
+
+
 from .serializers import Todo, TodoSerializer
+
 
 # Create your views here.
 class TodoView(ModelViewSet):
+
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    pagination_class = PageNumberPagination
+    
+
+    # Alternative temporary method
+    # PageNumberPagination.page_size = 25
+    # PageNumberPagination.page_size_query_param = 'numperpage'
+    # PageNumberPagination.page_query_param = 'pagenum'
