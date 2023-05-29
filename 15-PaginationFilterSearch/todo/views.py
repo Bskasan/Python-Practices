e09097bfef5797d28bbfedaa6edf4741d524f686
Bskasan <b.kasan@yandex.com>
2023-05-29 14:42:32 +0300

    local setting pagenumpag

--- 15-PaginationFilterSearch/todo/views.py
+++ 15-PaginationFilterSearch/todo/views.py
@@ -14,7 +14,7 @@ class TodoView(ModelViewSet):
 
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-    pagination_class = CustomPageNumberPagination
+    pagination_class = CustomPageNumberPagination # Local Setting for Todo View.
     
 
     # Alternative temporary method
