4acafe3bb97be7131fc1941863d5c899f3935959
Bskasan <b.kasan@yandex.com>
2023-06-01 16:04:00 +0300

    done create method - authDjango

--- 16-Django_Auth/user/serializers.py
+++ 16-Django_Auth/user/serializers.py
@@ -33,3 +33,16 @@ class RegistrationSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
             raise serializers.ValidationError({'password':"Password doesn't match!"})
+
+        return attrs
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            username = validated_data.get('username'),
+            email = validated_data.get('email'),
+        )
+
+        user.set_password(validated_data.get('password'))
+        user.save()
+
+        return super().create(validated_data)
