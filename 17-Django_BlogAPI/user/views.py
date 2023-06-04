eef155da1845d6b8e8148eaa7603acb4bcbd48d2
Bskasan <b.kasan@yandex.com>
2023-06-04 22:59:17 +0300

    user serializer and view done

--- 17-Django_BlogAPI/user/views.py
+++ 17-Django_BlogAPI/user/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import (
+    User, UserSerializer
+)
 
-# Create your views here.
+class UserView(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
