d7babb5471164da743f9f7a821e25522f0485baf
Bskasan <b.kasan@yandex.com>
2023-06-04 22:48:03 +0300

    blog done

--- 17-Django_BlogAPI/main/settings.py
+++ 17-Django_BlogAPI/main/settings.py
@@ -127,3 +127,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # 'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication'],
+    # 'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticatedOrReadOnly'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+    # 'DEFAULT_FILTER_BACKENDS': [
+    #   'django_filters.rest_framework.DjangoFilterBackend', 
+    #   'rest_framework.filters.SearchFilter',
+    #   'rest_framework.filters.OrderingFilter'
+    # ]
+}
