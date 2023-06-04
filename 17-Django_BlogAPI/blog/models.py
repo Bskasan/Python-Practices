f1fe4abdfa687d0c655c601fc2bba467556a43d1
Bskasan <b.kasan@yandex.com>
2023-06-04 22:08:45 +0300

    verbose_name - blogapi

--- 17-Django_BlogAPI/blog/models.py
+++ 17-Django_BlogAPI/blog/models.py
@@ -4,6 +4,15 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=64)
 
+    # class Meta:
+    #     verbose_name = 'Kategori'
+    #     verbose_name_plural = 'Blog Kategorileri'
+
+    def __str__(self):
+        return self.name
+    
+    
+
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -11,5 +20,7 @@ class Post(models.Model):
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-    category_id = models.BigIntegerField()
-    user_id = models.BigIntegerField()
+    
+
+    def __str__(self):
+        return self.category + ' / ' + self.title
