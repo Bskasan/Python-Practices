77a9cae4d946cf99057b910fa3374eeb156a35a3
Bskasan <b.kasan@yandex.com>
2023-06-04 22:03:40 +0300

    Written category and post models - many to  one rel

--- 17-Django_BlogAPI/blog/models.py
+++ 17-Django_BlogAPI/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    category_id = models.BigIntegerField()
+    user_id = models.BigIntegerField()
---
+++ 17-Django_BlogAPI/readme.md
@@ -0,0 +1,3 @@
+# Draw SQL - App Recommendation
+
+-> CASCADE means that Whatever you do in Category tables, do the same thing in Post Table.
