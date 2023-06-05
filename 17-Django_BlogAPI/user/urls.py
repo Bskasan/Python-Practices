7deadfa912a259d6dcb504f3cb926dd8b98be9bf
Bskasan <b.kasan@yandex.com>
2023-06-05 13:55:26 +0300

    obtain auth token

--- 17-Django_BlogAPI/user/urls.py
+++ 17-Django_BlogAPI/user/urls.py
@@ -3,11 +3,15 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserView
 
+# Built-in Sign up Function of Django Rest Framework
+from rest_framework.authtoken.views import obtain_auth_token
+
 router = DefaultRouter()
 router.register('', UserView)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('login', obtain_auth_token)
 ]
 
 
