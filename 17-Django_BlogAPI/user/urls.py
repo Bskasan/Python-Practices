e6a2e2e5091961e5fd9987883d627bd804e36787
Bskasan <b.kasan@yandex.com>
2023-06-05 14:31:36 +0300

    logout func

--- 17-Django_BlogAPI/user/urls.py
+++ 17-Django_BlogAPI/user/urls.py
@@ -6,12 +6,24 @@ from .views import UserView
 # Built-in Sign up Function of Django Rest Framework
 from rest_framework.authtoken.views import obtain_auth_token
 
+# logout Function
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET', 'POST'])
+def logout(request):
+    request.user.auth_token.delete()
+    return Response({"message": 'User Logout: Token Deleted!'})
+
+
 router = DefaultRouter()
 router.register('', UserView)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('login', obtain_auth_token)
+    path('login', obtain_auth_token),
+    path('logout', logout),
+
 ]
 
 
