f94184f71c284edd2fbc0aa25f543e7c5912b301
Bskasan <b.kasan@yandex.com>
2023-06-07 00:59:19 +0300

    env config decouple - flight api

--- 18-Django_FlightApp_API/core/settings/__init__.py
+++ 18-Django_FlightApp_API/core/settings/__init__.py
@@ -1,2 +1,8 @@
-from .base import *
-from .development import *
+from decouple import config
+
+ENVIRONMENT = config('ENV')
+
+if ENVIRONMENT == 'development':
+    from .development import *
+else:
+    from .production import *
