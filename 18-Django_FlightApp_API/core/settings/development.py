4a5524ffea4bc7d786f8351c0ade6c8dd04ee97c
Bskasan <b.kasan@yandex.com>
2023-06-06 20:11:55 +0300

    up - flight api

--- 18-Django_FlightApp_API/core/settings/development.py
+++ 18-Django_FlightApp_API/core/settings/development.py
@@ -12,3 +12,13 @@ INSTALLED_APPS += [
 MIDDLEWARE += [
      'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
--- 18-Django_FlightApp_API/core/settings/production.py
+++ 18-Django_FlightApp_API/core/settings/production.py
@@ -8,3 +8,18 @@ ALLOWED_HOSTS = ['127.0.0.1']
 INSTALLED_APPS += []
 
 MIDDLEWARE += []
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = { 
+    "default": { 
+        "ENGINE": "django.db.backends.postgresql_psycopg2", 
+        "NAME": config("SQL_DATABASE"), 
+        "USER": config("SQL_USER"), 
+        "PASSWORD": config("SQL_PASSWORD"), 
+        "HOST": config("SQL_HOST"), 
+        "PORT": config("SQL_PORT"), 
+        "ATOMIC_REQUESTS": True,
+    }
+} 
